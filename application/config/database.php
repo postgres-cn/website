f1e79aa88135cc23a263c736912efb25dbfa56e1
Daojing Zhou <doudou586@gmail.com>
2019-01-06 01:03:35 +0800

    Hide info

--- application/config/database.php
+++ application/config/database.php
@@ -48,27 +48,10 @@
 $active_group = 'default';
 $active_record = TRUE;
 
-/**
-$db['default']['hostname'] = 'localhost';
-$db['default']['username'] = '';
-$db['default']['password'] = '';
-$db['default']['database'] = '';
-$db['default']['dbdriver'] = 'mysql';
-$db['default']['dbprefix'] = '';
-$db['default']['pconnect'] = TRUE;
-$db['default']['db_debug'] = TRUE;
-$db['default']['cache_on'] = FALSE;
-$db['default']['cachedir'] = '';
-$db['default']['char_set'] = 'utf8';
-$db['default']['dbcollat'] = 'utf8_general_ci';
-$db['default']['swap_pre'] = '';
-$db['default']['autoinit'] = TRUE;
-$db['default']['stricton'] = FALSE;
-*/
 
 $db['default']['hostname'] = 'localhost';
-$db['default']['username'] = 'postgres';
-$db['default']['password'] = 'abcd';
+$db['default']['username'] = '********';
+$db['default']['password'] = '********';
 $db['default']['database'] = 'pgchina';
 $db['default']['dbdriver'] = 'postgre';
 $db['default']['dbprefix'] = '';
