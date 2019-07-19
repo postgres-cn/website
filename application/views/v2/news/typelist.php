9dcd5d5919e99d248affc96a86e07144390d00d9
Daojing. Zhou <doudou586@gmail.com>
2019-07-19 17:19:53 +0800

    关闭《顶》《踩》功能和显示
    关闭《顶》《踩》功能和显示

--- application/views/v2/news/typelist.php
+++ application/views/v2/news/typelist.php
@@ -62,8 +62,10 @@
 							"<i class='glyphicon glyphicon-remove'></i></a> &nbsp;" ;
 				}
 				?>
+			    <!-- 
                         <i class="glyphicon glyphicon-thumbs-up"></i> 顶：<span><?php echo $news['goodclicks']; ?></span>
                         &nbsp;<i class="glyphicon glyphicon-thumbs-down"></i> 踩：<span><?php echo $news['badclicks']; ?></span>
+				-->
                     </div>
                 </div>
             </div>
