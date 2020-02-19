6bc99482aea545aa170e8b4a41cb4af71274cbf2
Daojing Zhou <doudou586@gmail.com>
2020-02-19 16:08:01 +0800

    培训证书栏目修改版面布局和分类方式

--- application/views/v2/certificate/cert_p1.php
+++ application/views/v2/certificate/cert_p1.php
@@ -5,8 +5,8 @@
     <div class="col-md-2">
         <div class="well">
 			<ul class="nav nav-pills nav-stacked">
-                <li><a href="/v2/certificate">一期名单</a></li>
-                <li><a href="/v2/certificate/phase2">二期名单</a></li>
+                <li><a href="/v2/certificate">2019名单</a></li>
+                <li><a href="/v2/certificate/p2020">2020名单</a></li>
             </ul>
         </div>
 
@@ -18,65 +18,178 @@
             <ol class="breadcrumb">
                 <li><a href="#">首页</a></li>
                 <li><a href="#">培训认证</a></li>
-                <li class="active"><a href="#">一期通过认证人员</a></li>
+                <li class="active"><a href="#">2019年度通过认证人员</a></li>
             </ol>
             <!--社区信息-->
             <div class="clearfix">
                 <div class="col-sm-4">
-					<img src="/images/certs/phase1/窦培19092218111.jpg" onclick="window.open(this.src)" class="img-responsive img-rounded"  style="cursor:pointer;height:240px;"></img>
+					<img src="/images/certs/2019/窦培19092218111.jpg" onclick="window.open(this.src)" class="img-responsive img-rounded"  style="cursor:pointer;height:240px;"></img>
                 </div>
                 <div class="col-sm-4">
-					<img src="/images/certs/phase1/冯超19090432105.jpg" onclick="window.open(this.src)" class="img-responsive img-rounded"  style="cursor:pointer;height:240px;"></img>
+					<img src="/images/certs/2019/冯超19090432105.jpg" onclick="window.open(this.src)" class="img-responsive img-rounded"  style="cursor:pointer;height:240px;"></img>
                 </div>
                 <div class="col-sm-4">
-					<img src="/images/certs/phase1/高永超19091510106.jpg" onclick="window.open(this.src)" class="img-responsive img-rounded"  style="cursor:pointer;height:240px;"></img>
+					<img src="/images/certs/2019/高永超19091510106.jpg" onclick="window.open(this.src)" class="img-responsive img-rounded"  style="cursor:pointer;height:240px;"></img>
                 </div>
 
                 <div class="col-sm-4">
-					<img src="/images/certs/phase1/何敏19096732108.jpg" onclick="window.open(this.src)" class="img-responsive img-rounded"  style="cursor:pointer;height:240px;"></img>
+					<img src="/images/certs/2019/何敏19096732108.jpg" onclick="window.open(this.src)" class="img-responsive img-rounded"  style="cursor:pointer;height:240px;"></img>
                 </div>
                 <div class="col-sm-4">
-					<img src="/images/certs/phase1/李敬财19095912115.jpg" onclick="window.open(this.src)" class="img-responsive img-rounded"  style="cursor:pointer;height:240px;"></img>
+					<img src="/images/certs/2019/李敬财19095912115.jpg" onclick="window.open(this.src)" class="img-responsive img-rounded"  style="cursor:pointer;height:240px;"></img>
                 </div>
                 <div class="col-sm-4">
-					<img src="/images/certs/phase1/马文豪19091239104.jpg" onclick="window.open(this.src)" class="img-responsive img-rounded"  style="cursor:pointer;height:240px;"></img>
-                </div>
-			
-                <div class="col-sm-4">
-					<img src="/images/certs/phase1/彭占元19095126102.jpg" onclick="window.open(this.src)" class="img-responsive img-rounded"  style="cursor:pointer;height:240px;"></img>
-                </div>
-                <div class="col-sm-4">
-					<img src="/images/certs/phase1/秦将华19092510109.jpg" onclick="window.open(this.src)" class="img-responsive img-rounded"  style="cursor:pointer;height:240px;"></img>
-                </div>
-                <div class="col-sm-4">
-					<img src="/images/certs/phase1/邱文辉19093517107.jpg" onclick="window.open(this.src)" class="img-responsive img-rounded"  style="cursor:pointer;height:240px;"></img>
+					<img src="/images/certs/2019/马文豪19091239104.jpg" onclick="window.open(this.src)" class="img-responsive img-rounded"  style="cursor:pointer;height:240px;"></img>
                 </div>
 			
                 <div class="col-sm-4">
-					<img src="/images/certs/phase1/舒正英19098513112.jpg" onclick="window.open(this.src)" class="img-responsive img-rounded"  style="cursor:pointer;height:240px;"></img>
+					<img src="/images/certs/2019/彭占元19095126102.jpg" onclick="window.open(this.src)" class="img-responsive img-rounded"  style="cursor:pointer;height:240px;"></img>
                 </div>
                 <div class="col-sm-4">
-					<img src="/images/certs/phase1/孙雅艺19096065107.jpg" onclick="window.open(this.src)" class="img-responsive img-rounded"  style="cursor:pointer;height:240px;"></img>
+					<img src="/images/certs/2019/秦将华19092510109.jpg" onclick="window.open(this.src)" class="img-responsive img-rounded"  style="cursor:pointer;height:240px;"></img>
                 </div>
                 <div class="col-sm-4">
-					<img src="/images/certs/phase1/唐林19095516114.jpg" onclick="window.open(this.src)" class="img-responsive img-rounded"  style="cursor:pointer;height:240px;"></img>
+					<img src="/images/certs/2019/邱文辉19093517107.jpg" onclick="window.open(this.src)" class="img-responsive img-rounded"  style="cursor:pointer;height:240px;"></img>
+                </div>
+			
+                <div class="col-sm-4">
+					<img src="/images/certs/2019/舒正英19098513112.jpg" onclick="window.open(this.src)" class="img-responsive img-rounded"  style="cursor:pointer;height:240px;"></img>
+                </div>
+                <div class="col-sm-4">
+					<img src="/images/certs/2019/孙雅艺19096065107.jpg" onclick="window.open(this.src)" class="img-responsive img-rounded"  style="cursor:pointer;height:240px;"></img>
+                </div>
+                <div class="col-sm-4">
+					<img src="/images/certs/2019/唐林19095516114.jpg" onclick="window.open(this.src)" class="img-responsive img-rounded"  style="cursor:pointer;height:240px;"></img>
                 </div>
 
 			    <div class="col-sm-4">
-					<img src="/images/certs/phase1/于成智1909619110.jpg" onclick="window.open(this.src)" class="img-responsive img-rounded"  style="cursor:pointer;height:240px;"></img>
+					<img src="/images/certs/2019/于成智1909619110.jpg" onclick="window.open(this.src)" class="img-responsive img-rounded"  style="cursor:pointer;height:240px;"></img>
                 </div>
                 <div class="col-sm-4">
-					<img src="/images/certs/phase1/于天恒19096018116.jpg" onclick="window.open(this.src)" class="img-responsive img-rounded"  style="cursor:pointer;height:240px;"></img>
+					<img src="/images/certs/2019/于天恒19096018116.jpg" onclick="window.open(this.src)" class="img-responsive img-rounded"  style="cursor:pointer;height:240px;"></img>
                 </div>
                 <div class="col-sm-4">
-					<img src="/images/certs/phase1/张肖飞19094575113.jpg" onclick="window.open(this.src)" class="img-responsive img-rounded"  style="cursor:pointer;height:240px;"></img>
+					<img src="/images/certs/2019/张肖飞19094575113.jpg" onclick="window.open(this.src)" class="img-responsive img-rounded"  style="cursor:pointer;height:240px;"></img>
                 </div>
 
 			
 			    <div class="col-sm-4">
-					<img src="/images/certs/phase1/赵云龙19096615103.jpg" onclick="window.open(this.src)" class="img-responsive img-rounded"  style="cursor:pointer;height:240px;"></img>
+					<img src="/images/certs/2019/赵云龙19096615103.jpg" onclick="window.open(this.src)" class="img-responsive img-rounded"  style="cursor:pointer;height:240px;"></img>
+                </div>
+
+<!-- -->			
+                <div class="col-sm-4">
+					<img src="/images/certs/2019/何勇19107034213.jpg" onclick="window.open(this.src)" class="img-responsive img-rounded"  style="cursor:pointer;height:240px;"></img>
+                </div>
+                <div class="col-sm-4">
+					<img src="/images/certs/2019/胡璨19106010208.jpg" onclick="window.open(this.src)" class="img-responsive img-rounded"  style="cursor:pointer;height:240px;"></img>
+                </div>
+                <div class="col-sm-4">
+					<img src="/images/certs/2019/见永康19107890211.jpg" onclick="window.open(this.src)" class="img-responsive img-rounded"  style="cursor:pointer;height:240px;"></img>
+                </div>
+
+                <div class="col-sm-4">
+					<img src="/images/certs/2019/李耀宗19101832222.jpg" onclick="window.open(this.src)" class="img-responsive img-rounded"  style="cursor:pointer;height:240px;"></img>
+                </div>
+                <div class="col-sm-4">
+					<img src="/images/certs/2019/林根19106710220.jpg" onclick="window.open(this.src)" class="img-responsive img-rounded"  style="cursor:pointer;height:240px;"></img>
+                </div>
+                <div class="col-sm-4">
+					<img src="/images/certs/2019/刘邦艺19107217201.jpg" onclick="window.open(this.src)" class="img-responsive img-rounded"  style="cursor:pointer;height:240px;"></img>
                 </div>
 			
+                <div class="col-sm-4">
+					<img src="/images/certs/2019/刘雪柏19100412205.jpg" onclick="window.open(this.src)" class="img-responsive img-rounded"  style="cursor:pointer;height:240px;"></img>
+                </div>
+                <div class="col-sm-4">
+					<img src="/images/certs/2019/刘用19102112206.jpg" onclick="window.open(this.src)" class="img-responsive img-rounded"  style="cursor:pointer;height:240px;"></img>
+                </div>
+                <div class="col-sm-4">
+					<img src="/images/certs/2019/罗文芳19103856210.jpg" onclick="window.open(this.src)" class="img-responsive img-rounded"  style="cursor:pointer;height:240px;"></img>
+                </div>
+			
+                <div class="col-sm-4">
+					<img src="/images/certs/2019/齐红涛19103818209.jpg" onclick="window.open(this.src)" class="img-responsive img-rounded"  style="cursor:pointer;height:240px;"></img>
+                </div>
+                <div class="col-sm-4">
+					<img src="/images/certs/2019/石敦斌19103610221.jpg" onclick="window.open(this.src)" class="img-responsive img-rounded"  style="cursor:pointer;height:240px;"></img>
+                </div>
+                <div class="col-sm-4">
+					<img src="/images/certs/2019/石勇虎19107015212.jpg" onclick="window.open(this.src)" class="img-responsive img-rounded"  style="cursor:pointer;height:240px;"></img>
+                </div>
+
+			    <div class="col-sm-4">
+					<img src="/images/certs/2019/宋子朋19101417207.jpg" onclick="window.open(this.src)" class="img-responsive img-rounded"  style="cursor:pointer;height:240px;"></img>
+                </div>
+                <div class="col-sm-4">
+					<img src="/images/certs/2019/王亭19102144216.jpg" onclick="window.open(this.src)" class="img-responsive img-rounded"  style="cursor:pointer;height:240px;"></img>
+                </div>
+                <div class="col-sm-4">
+					<img src="/images/certs/2019/杨阳子19106124203.jpg" onclick="window.open(this.src)" class="img-responsive img-rounded"  style="cursor:pointer;height:240px;"></img>
+                </div>
+
+			
+			    <div class="col-sm-4">
+					<img src="/images/certs/2019/张飞雄19101714204.jpg" onclick="window.open(this.src)" class="img-responsive img-rounded"  style="cursor:pointer;height:240px;"></img>
+                </div>
+			    <div class="col-sm-4">
+					<img src="/images/certs/2019/张婷婷19100209218.jpg" onclick="window.open(this.src)" class="img-responsive img-rounded"  style="cursor:pointer;height:240px;"></img>
+                </div>
+			    <div class="col-sm-4">
+					<img src="/images/certs/2019/赵玉珮19104144202.jpg" onclick="window.open(this.src)" class="img-responsive img-rounded"  style="cursor:pointer;height:240px;"></img>
+                </div>
+
+			    <div class="col-sm-4">
+					<img src="/images/certs/2019/周国超19103013215.jpg" onclick="window.open(this.src)" class="img-responsive img-rounded"  style="cursor:pointer;height:240px;"></img>
+                </div>
+			    <div class="col-sm-4">
+					<img src="/images/certs/2019/周辉19108294214.jpg" onclick="window.open(this.src)" class="img-responsive img-rounded"  style="cursor:pointer;height:240px;"></img>
+                </div>
+			    <div class="col-sm-4">
+					<img src="/images/certs/2019/朱贵平19106894219.jpg" onclick="window.open(this.src)" class="img-responsive img-rounded"  style="cursor:pointer;height:240px;"></img>
+                </div>			
+
+			    <div class="col-sm-4">
+					<img src="/images/certs/2019/朱丽云19100720217.jpg" onclick="window.open(this.src)" class="img-responsive img-rounded"  style="cursor:pointer;height:240px;"></img>
+                </div>		
+
+
+			    <div class="col-sm-4">
+					<img src="/images/certs/2019/陈阵19110722606.jpg" onclick="window.open(this.src)" class="img-responsive img-rounded"  style="cursor:pointer;height:240px;"></img>
+                </div>	
+			    <div class="col-sm-4">
+					<img src="/images/certs/2019/姜柳19110722602.jpg" onclick="window.open(this.src)" class="img-responsive img-rounded"  style="cursor:pointer;height:240px;"></img>
+                </div>	
+			    <div class="col-sm-4">
+					<img src="/images/certs/2019/李士勇19110722607.jpg" onclick="window.open(this.src)" class="img-responsive img-rounded"  style="cursor:pointer;height:240px;"></img>
+                </div>	
+
+
+			    <div class="col-sm-4">
+					<img src="/images/certs/2019/鲁山19110722601.jpg" onclick="window.open(this.src)" class="img-responsive img-rounded"  style="cursor:pointer;height:240px;"></img>
+                </div>	
+			    <div class="col-sm-4">
+					<img src="/images/certs/2019/王克光19110722603.jpg" onclick="window.open(this.src)" class="img-responsive img-rounded"  style="cursor:pointer;height:240px;"></img>
+                </div>	
+			    <div class="col-sm-4">
+					<img src="/images/certs/2019/王庆磊19110722605.jpg" onclick="window.open(this.src)" class="img-responsive img-rounded"  style="cursor:pointer;height:240px;"></img>
+                </div>	
+
+			    <div class="col-sm-4">
+					<img src="/images/certs/2019/王向华19110722621.jpg" onclick="window.open(this.src)" class="img-responsive img-rounded"  style="cursor:pointer;height:240px;"></img>
+                </div>	
+			    <div class="col-sm-4">
+					<img src="/images/certs/2019/王玉华19110722604.jpg" onclick="window.open(this.src)" class="img-responsive img-rounded"  style="cursor:pointer;height:240px;"></img>
+                </div>	
+			    <div class="col-sm-4">
+					<img src="/images/certs/2019/马留强19120722228.jpg" onclick="window.open(this.src)" class="img-responsive img-rounded"  style="cursor:pointer;height:240px;"></img>
+                </div>	
+
+			    <div class="col-sm-4">
+					<img src="/images/certs/2019/任玉龙19120722221.jpg" onclick="window.open(this.src)" class="img-responsive img-rounded"  style="cursor:pointer;height:240px;"></img>
+                </div>	
+
+
 			</div>
 
 
