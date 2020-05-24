1c2d1abaa49d35a41586a44abcba9d6e7a3c9259
Daojing Zhou <doudou586@gmail.com>
2020-05-24 10:35:02 +0800

    将首页下方的外部网站链接、QQ群链接等修改为白色字。

--- application/views/v2/templates/footer.php
+++ application/views/v2/templates/footer.php
@@ -8,24 +8,24 @@
     <div class="container" style="padding: 20px">
         <div class="col-md-1"></div>
         <div class="col-md-3">
-            <p><strong>加入我们：</strong></p>
-            <ul class="QQnum">
+            <p class="text-white" style="color:#fff"><strong>加入我们：</strong></p>
+            <ul class="QQnum text-white"  style="color:#fff">
                 <li>QQ群1：3336901</li>
                 <li>QQ群2：254622631</li>
                 <li>文档群：150657323</li>
             </ul>
         </div>
         <div class="col-md-3 support">
-            <p><a href="#"><strong>商业支持：</strong></a></p>
-            <ul class="QQnum">
-                <li><a target=_blank href="http://w3.ww-it.cn/">成都文武信息技术有限公司</a></li>
-                <li><a target=_blank href="http://www.cstech.ltd/">杭州乘数科技有限公司</a></li>
-                <li><a target=_blank  href="https://www.aliyun.com/product/rds/postgresql?spm=5176.198144.cloudEssentials.44.b68d346dRG9UL4">阿里云</a></li>
-                <li><a target=_blank href=" https://www.qingcloud.com/products/postgresql/">青云(北京优帆科技有限公司)</a></li>
+            <p class="text-white" style="color:#fff"><strong>商业支持：</strong></p>
+            <ul class="QQnum text-white">
+                <li><a target=_blank  style="color:#fff" href="http://w3.ww-it.cn/">成都文武信息技术有限公司</a></li>
+                <li><a target=_blank  style="color:#fff" href="http://www.cstech.ltd/">杭州乘数科技有限公司</a></li>
+                <li><a target=_blank  style="color:#fff" href="https://www.aliyun.com/product/rds/postgresql?spm=5176.198144.cloudEssentials.44.b68d346dRG9UL4">阿里云</a></li>
+                <li><a target=_blank  style="color:#fff" href=" https://www.qingcloud.com/products/postgresql/">青云(北京优帆科技有限公司)</a></li>
             </ul>
         </div>
         <div class="col-md-3 ">
-            <p><a href="#" style="color: #333;"><strong>扫码关注：</strong></a></p>
+            <p class="text-white" style="color:#fff"><strong>扫码关注：</strong></p>
             <div class="wechat_jpg">
                 <img src="/image/wechat.jpg" alt="扫描关注微信">
                 <a target=_blank href='http://weibo.com/postgresqlchina'><img src="/image/weibo.jpg" alt="扫描关注微博"></a>
