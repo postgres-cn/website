68bf84d8ac00bbf8f9bc655e10d65f1bcdaa5b38
Daojing Zhou <doudou586@gmail.com>
2020-08-03 16:44:08 +0800

    新增V12.2文档链接内容；

--- application/views/v2/document/document.php
+++ application/views/v2/document/document.php
@@ -27,6 +27,7 @@
                     <input type="text" name="q" value="" placeholder="关键字1 关键字2" class="form-control"/>
 					<select name="u" class="form-control">
 						<option value="/">不限版本</option>
+						<option value="/docs/12">只限12版本</option>
 						<option value="/docs/11">只限11版本</option>
 						<option value="/docs/10">只限10版本</option>
 						<option value="/docs/9.6">只限9版本</option>
@@ -39,9 +40,12 @@
 
             <!--文档列表-->
             <div class="well well-sm" >
-                <h4><a target=_blank href="/docs/11/">PostgreSQL 11.2 版本在线手册 ...(中文版本)</a></h4>
-                <p>感谢翻译小组(彭煜玮、车岩磊、陈华军、王昊、彭金金、徐小春、赵宇等)的辛苦工作。</p>
+                <h4><a target=_blank href="/docs/12/">PostgreSQL 12.2 版本在线手册 ...(中文版本)</a></h4>
+                <p>感谢翻译小组(车岩磊、赵宇、徐小春、陈华军、彭金金、齐瑞强等)的辛苦工作。</p>
                 <img src="/image/new_mark.jpg" width="50" style="position: absolute;right: -3px;top: -3px;"><!--新的内容右角标-->
+            </div>            
+            <div class="well well-sm" >
+                <h4><a target=_blank href="/docs/11/">PostgreSQL 11.2 版本在线手册 ...(中文版本)</a></h4>
             </div>            
             <div class="well well-sm" >
                 <h4><a target=_blank href="/docs/10/">PostgreSQL 10.1 版本在线手册 ...(中文版本)</a></h4>
